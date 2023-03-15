fd4e56eb074a60675ff2d806289502ecc32afce1
luvega-rj <luvegaronny@gmail.com>
2023-03-15 14:01:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(a){
   // Write your code here!
-}
+  let result;
+  if (a <= 400){
+   return result = 'This one is on me!';
+  }
+  else if (a <= 2499){
+   return result = 'I will gladly take your thirty bucks.';
+  }
+  else  (a > 2500);{
+   return result ='No can do.'
+  }
+  }
+  scuberGreetingForFeet(400)
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function ternaryCheckCity(b){
+    // Write your code here!
+    let result;
+    if (b === 'NYC'){
+      return result = 'Ok, sounds good.'
+    }
+    else {
+      return result = 'No go.'
+    }
+  }
+  ternaryCheckCity('Pittsburgh')
+  function switchOnCharmFromTip(c){
+    // Write your code here!
+    let result;
+    if (c === 'generous'){
+      return result = 'Thank you so much.'
+    }
+    else if (c === 'not as generous'){
+      return result = 'Thank you.'
+    }
+    else {
+      return result = 'Bye.'
+    }
+  }
+  switchOnCharmFromTip(generous)
